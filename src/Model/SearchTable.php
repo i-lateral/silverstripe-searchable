187ee47db5e35f764924185680cdf15c824ebb1c
Morven Lewis-Everley <morven@i-lateral.com>
2021-01-20 21:13:28 +0000

    Run impolrt task on dev/build (if needed)

--- src/Model/SearchTable.php
+++ src/Model/SearchTable.php
@@ -3,16 +3,31 @@
 namespace ilateral\SilverStripe\Searchable\Model;
 
 use SilverStripe\ORM\DataObject;
-use SilverStripe\ORM\Connect\MySQLSchemaManager;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Core\Injector\Injector;
+use ilateral\SilverStripe\Searchable\Tasks\ImportSearchDataTask;
 
 /**
  * Base table to store searchable data in
  */
 class SearchTable extends DataObject
 {
-    private static $table_name = 'SearchTable';
+    private static $table_name = 'Searchable_SearchTable';
 
     private static $has_one = [
         'BaseObject' => DataObject::class
     ];
+
+    public function requireDefaultRecords()
+    {
+        parent::requireDefaultRecords();
+
+        $run_migration = ImportSearchDataTask::config()->run_during_dev_build;
+        $existing = self::get()->exists();
+
+        if (!$existing && $run_migration) {
+            $request = Injector::inst()->get(HTTPRequest::class);
+            ImportSearchDataTask::create()->run($request);
+        }
+    }
 }
